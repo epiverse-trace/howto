a298b4abafe091aa627cb394576d98c10c9a06bc
Andree Valle Campos <avallecam@gmail.com>
2025-07-31 16:23:08 +0100

    add helper install script for renv

---
+++ internal/install.R
@@ -0,0 +1,5 @@
+# using strategy to make these packages visible to renv
+# https://carpentries.github.io/sandpaper/articles/building-with-renv.html#adding-new-packages-to-the-cache
+library(cli)
+library(downlit)
+library(xml2)
